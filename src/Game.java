0b431a905358d62a3c48e8da3839f5864ac72d71
ThirstyGit <60189553+ThirstyGit@users.noreply.github.com>
2020-01-26 23:02:39 +0600

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Game extends Actions {
@@ -36,6 +38,51 @@ public class Game extends Actions {
         frame.setResizable(false);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+    //Inner Class for button Actions
+    //ActionListener interface implemented
+    class Listener implements ActionListener {
+
+        @Override
+        //sets the image to whatever the state is
+        public void actionPerformed(ActionEvent actionEvent) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (actionEvent.getSource() == boxes[i][j]) {
+                        //Adding the icons based on the state of the box. i.e. no. of neighbors with bombs
+                        if (boxes[i][j].state == -1) { //You lose the game in this case.
+                            boxes[i][j].setIcon(new ImageIcon("mine.png"));
+                            JOptionPane.showMessageDialog(null, "You Lose");
+                            frame.dispose();
+                        }
+                        else {
+                            if (boxes[i][j].state == 0) {
+                                boxes[i][j].setVisible(false);
+                            } else if (boxes[i][j].state == 1) {
+                                boxes[i][j].setIcon(new ImageIcon("1.png"));
+                            } else if (boxes[i][j].state == 2) {
+                                boxes[i][j].setIcon(new ImageIcon("2.png"));
+                            } else if (boxes[i][j].state == 3) {
+                                boxes[i][j].setIcon(new ImageIcon("3.png"));
+                            } else if (boxes[i][j].state == 4) {
+                                boxes[i][j].setIcon(new ImageIcon("4.png"));
+                            } else if (boxes[i][j].state == 5) {
+                                boxes[i][j].setIcon(new ImageIcon("5.png"));
+                            } else if (boxes[i][j].state == 6) {
+                                boxes[i][j].setIcon(new ImageIcon("6.png"));
+                            } else if (boxes[i][j].state == 7) {
+                                boxes[i][j].setIcon(new ImageIcon("7.png"));
+                            } else if (boxes[i][j].state == 8) {
+                                boxes[i][j].setIcon(new ImageIcon("8.png"));
+                            }
+                            noOfBoxesLeft--;
+                        }
+                        if(noOfBoxesLeft == 0) JOptionPane.showMessageDialog(null, "You Win!");
+                    }
+                }
+            }
+        }
     }
 
     //Main method
